088726854cc652e1cee204711ca0b8a470364f8d
dortmundia <csulienjatek@gmail.com>
2020-12-11 13:42:33 +0100

    Add project files.

---
+++ Jarmuvek/Jarmu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarmuvek
+{
+    /*
+        Készíts egy Gépjármű absztrakt osztályt amely az alábbi adatokkal rendelkezik:
+            Üzemanyag
+            Kerekek száma
+            Rendszám
+     */
+   public abstract class Jarmu 
+    {
+
+        public int Uzemanyag;
+        public int Kerekek;
+        public string Rendszam;
+
+
+    }
+}
